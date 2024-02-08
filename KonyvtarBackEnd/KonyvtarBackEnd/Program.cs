4a0059ac52c13e8929afce81b979e062ae1243c2
Oláh Márk <OlahM@WINDOWS-3E3VDMB.mitiszkad.lan>
2024-02-08 12:07:57 +0100

    New Port access

--- KonyvtarBackEnd/KonyvtarBackEnd/Program.cs
+++ KonyvtarBackEnd/KonyvtarBackEnd/Program.cs
@@ -31,7 +31,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: "Niggerveszedelem",
                       builder =>
                       {
-                          builder.WithOrigins("http://127.0.0.1:5500","http://localhost:3000")
+                          builder.WithOrigins("http://127.0.0.1:5500","http://localhost:3000","http://localhost:3001")
                                  .AllowAnyMethod()
                                  .AllowAnyHeader();
                       });
--- KonyvtarBackEnd/KonyvtarBackEnd/obj/Debug/net7.0/KonyvtarBackEnd.AssemblyInfo.cs
+++ KonyvtarBackEnd/KonyvtarBackEnd/obj/Debug/net7.0/KonyvtarBackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KonyvtarBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fb733be6de69f94dc4349b526e4d5c893766acc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("KonyvtarBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KonyvtarBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
