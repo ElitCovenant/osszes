0776076e9566744c9c35b5e77f8b018073f2b713
ElitCovenant <szovetsege@gmail.com>
2024-03-18 08:36:05 +0100

    Admin email sender added to myprofile

---
+++ lbp_Teljes/src/Email.js
@@ -0,0 +1,133 @@
+import React, { useState, useEffect } from 'react';
+import './Email.css';
+
+function Loan() {
+  const [newEmail, setNewEmail] = useState('');
+  const [emailList, setEmailList] = useState([]);
+  const [message, setMessage] = useState('');
+  const [recipients, setRecipients] = useState('');
+  const [file, setFile] = useState(null);
+  const [isPanelVisible, setIsPanelVisible] = useState(true); // Új állapot a panel láthatóságának kezelésére
+
+
+
+  // E-mail cím validálás
+  const isValidEmail = email => /\S+@\S+\.\S+/.test(email);
+
+  useEffect(() => {
+    // Az e-mail címek lekérése a komponens betöltésekor
+    const fetchEmails = async () => {
+      try {
+        const response = await fetch('/api/get-emails');
+        if (!response.ok) throw new Error('Hiba történt az e-mail címek lekérése közben.');
+        const emails = await response.json();
+        setEmailList(emails);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+    fetchEmails();
+  }, []);
+
+  const saveEmail = async (email) => {
+    try {
+      const response = await fetch('https://localhost:7275/User', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email }),
+      });
+      if (!response.ok) throw new Error('Hiba történt az e-mail cím mentése közben.');
+      // Sikeres mentés után frissítheted a lokális állapotot vagy visszajelzést adhatsz a felhasználónak
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const addEmail = () => {
+    if (newEmail && isValidEmail(newEmail) && !emailList.includes(newEmail)) {
+      setEmailList(prevEmails => [...prevEmails, newEmail]);
+      saveEmail(newEmail);
+      setNewEmail('');
+    } else {
+      alert("Kérjük, adjon meg egy érvényes e-mail címet!");
+    }
+  };
+
+  const removeEmail = index => {
+    setEmailList(emailList.filter((_, i) => i !== index));
+  };
+
+  const sendMessage = async () => {
+    try {
+      const senderemail = {
+        to: recipients,
+        subject: "Könyvtár kölcsönzés",
+        body: message
+      };
+  
+      const response = await fetch('https://localhost:7275/Email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(senderemail)
+      });
+  
+      if (!response.ok) {
+        throw new Error('Hiba történt az üzenet küldése közben.');
+      }
+  
+      alert("Üzenet elküldve!");
+    } catch (error) {
+      console.error('Hiba történt az üzenet küldése közben:', error.message);
+    }
+  };
+
+  const prepareMessage = () => {
+    const defaultMessage = "Kedves Olvasónk! Ne feledje, hogy a kölcsönzött könyv(ek) visszahozatali határideje hamarosan lejár.";
+    setMessage(defaultMessage);
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setFile(file);
+    // Itt lehetne a fájl tartalmát kezelni, például olvasással vagy egy szerverre való feltöltéssel
+  };
+
+  
+  
+  const handleClose = () => {
+    setIsPanelVisible(false); // Beállítja a panel láthatóságát hamisra
+  };
+
+  if (!isPanelVisible) return null;
+
+
+
+  return (
+    <div className="panel">
+      <div className="message-window">
+        <input type="text" placeholder="Adja meg a címzettek e-mail címét itt..." value={recipients} onChange={(e) => setRecipients(e.target.value)} />
+        <textarea placeholder="Írjon üzenetet itt..." value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+        <button onClick={sendMessage}>Üzenet küldése</button>
+        <button className='prepare-message' onClick={prepareMessage}>Üzenet Előkészítése</button>
+        <button className='close-panel' onClick={handleClose}>Bezárás</button>
+      </div>
+      {/* <div className="email-list-window">
+        <input type="text" placeholder="Új e-mail cím hozzáadása..." value={newEmail} onChange={(e) => setNewEmail(e.target.value)} />
+        <button onClick={addEmail}>Hozzáad</button>
+        <ul>
+          {emailList.map((email, index) => (
+            <li key={index}>{email} <button onClick={() => removeEmail(index)}>Törlés</button></li>
+          ))}
+        </ul>
+      </div> */}
+      {/* { <div>
+        <input type="file" onChange={handleFileChange} />
+        {file && <p>Feltöltött fájl: {file.name}</p>}
+      </div> } */}
+    </div>
+  );
+}
+
+export default Loan;
--- lbp_Teljes/src/UserPage.js
+++ lbp_Teljes/src/UserPage.js
@@ -1,25 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './UserPage.css';
-import AvatarSelector from './AvatarSelector'; // Assuming this component has already been implemented
+import AvatarSelector from './AvatarSelector'; // Feltételezve, hogy ez a komponens már megíródott
 import jwt_decode from './jwt_decode';
 import def_logo from './img/default_prof_picture.png';
 import quest1_logo from './img/quest1_prof_picture.png';
 import quest2_logo from './img/quest2_prof_picture.png';
 import teacher1_logo from './img/teacher1_prof_picture.png';
 import teacher2_logo from './img/teacher2_prof_picture.png';
+import Email from './Email';
 
 function UserPage() {
-  const avatarlogos = [def_logo, teacher1_logo, teacher2_logo, quest1_logo, quest2_logo];
-  const [isMailboxOpen, setIsMailboxOpen] = useState(false);
+  const avatarlogos = [def_logo, teacher1_logo, teacher2_logo, quest1_logo, quest2_logo]
   const [isRoleSelectorOpen, setIsRoleSelectorOpen] = useState(false);
   const [profilePicturePath, setProfilePicturePath] = useState(null);
-  const [decodedEmail, setDecodedEmail] = useState('');
-  const [decodedrole, setDecodedRole] = useState('');
-
-  const toggleMailbox = (e) => {
-    e.stopPropagation(); // Prevent click event propagation
-    setIsMailboxOpen(!isMailboxOpen);
-  };
+  const [decodedEmail, setDecodedEmail] = useState("");
+  const [decodedrole, setDecodedRole] = useState("");
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [isLoanVisible, setIsLoanVisible] = useState(false);
 
   const toggleRoleSelector = () => {
     setIsRoleSelectorOpen(!isRoleSelectorOpen);
@@ -29,69 +26,60 @@ function UserPage() {
     setIsRoleSelectorOpen(false);
   };
 
+  const toggleLoan = () => {
+    setIsLoanVisible(!isLoanVisible);
+  };
+
   useEffect(() => {
     const fetchProfilePicturePath = async () => {
       try {
-        const troken = localStorage.getItem('authToken');
-        if (troken) {
-          const decodedToken = jwt_decode(troken);
+        const token = localStorage.getItem('authToken');
+        if (token) {
+          const decodedToken = jwt_decode(token);
+          setDecodedRole(decodedToken.role === "Admin" ? "Librarian" : "Student");
+          setIsAdmin(decodedToken.role === "Admin");
+          setDecodedEmail(decodedToken.emailaddress.split("@")[0].toUpperCase());
           const response = await fetch(`https://localhost:7275/önkép/${decodedToken.dns}`);
           if (response.ok) {
             const data = await response.json();
             if (data != null) {
               setProfilePicturePath(data);
-              setDecodedEmail(decodedToken.emailaddress.split('@')[0].toUpperCase());
-              setDecodedRole(decodedToken.role === 'Admin' ? 'Librarian' : 'Student');
             } else {
-              console.error('No profile picture found in the data');
+              console.error('Nincs profilkép az adatokban');
             }
           } else {
-            console.error('Error fetching profile picture:', response.status);
+            console.error('Hiba történt a profilkép lekérésekor:', response.status);
           }
         }
       } catch (error) {
-        console.error('Error fetching profile picture:', error);
+        console.error('Error fetching profile picture path:', error);
       }
     };
 
     fetchProfilePicturePath();
   }, []);
 
-  const handleLibraryManagerDownload = () => {
-    // Create an anchor element
-    const anchor = document.createElement('a');
-    // Set href to the URL of the MSI file
-    anchor.href = 'http://img.library.nhely.hu/setup/KonyvtarKarbantarto_Setup.msi';
-    // Set the download attribute and the filename
-    anchor.download = 'KonyvtarKarbantarto_Setup.msi';
-    // Simulate a click on the anchor element
-    anchor.click();
-  };
-
   return (
-    <div className="facebook-profile">
-      <div className="profile-header" onClick={toggleRoleSelector}>
-        <img src={profilePicturePath > 0 ? avatarlogos[profilePicturePath - 1] : avatarlogos[profilePicturePath]} alt="Profile" height={80} />
-        <div>
-          <h2>Welcome {decodedEmail}</h2>
-          <p>{decodedrole}</p>
-        </div>
-        <img src="faEmail.png" alt="Mailbox" className="mail-icon" onClick={toggleMailbox} />
-      </div>
-      {isRoleSelectorOpen && (
-        <>
-          <div className="role-selector-modal-overlay" onClick={toggleRoleSelector}></div>
-          <div className="role-selector-modal">
-            <AvatarSelector onClose={confirmRoleSelection} />
+    <div>
+      <div className="facebook-profile">
+        <div className="profile-header" onClick={toggleRoleSelector}>
+          <img src={profilePicturePath > 0 ? avatarlogos[profilePicturePath - 1] : avatarlogos[profilePicturePath]} alt="Profile" height={80} />
+          <div>
+            <h2>Welcome {decodedEmail}</h2>
+            <p>{decodedrole}</p>
           </div>
-        </>
-      )}
-      {isMailboxOpen && (
-        <div className="mailbox">
-          <button onClick={handleLibraryManagerDownload}>Library Manager Download</button>
-          <button onClick={toggleMailbox}>Close</button>
         </div>
-      )}
+        {isRoleSelectorOpen && (
+          <>
+            <div className="role-selector-modal-overlay" onClick={toggleRoleSelector}></div>
+            <div className="role-selector-modal">
+              <AvatarSelector onClose={confirmRoleSelection} />
+            </div>
+          </>
+        )}
+        {isAdmin && <button className="toggleLoan-button" onClick={toggleLoan}>Email</button>}
+      </div>
+      {isLoanVisible && <Email />}
     </div>
   );
 }
